91b97f9a210b46262f427a1dba49e8b2e2b735a4
hu8813 <h0553559@gmail.com>
2024-01-24 16:14:36 +0100

    a

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,26 @@
 // Footer.js
 import React from 'react';
-import { AiFillHeart } from 'react-icons/ai'; // Import Bootstrap icons
-import { GrInstagram, GrTwitter, GrFacebookOption } from 'react-icons/gr';
+import { AiFillHeart } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer-container">
-  
+      <div className="footer-links">
+        <Link to="/privacy-policy" className="footer-link">
+          Privacy Policy
+        </Link>
+        <Link to="/contact" className="footer-link">
+          Impressum
+        </Link>
+       
+      </div>
+
       <div className="footer-text">
         Made with <AiFillHeart className="heart-icon" /> by 42 Students
       </div>
-      
+ 
     </footer>
   );
 };
