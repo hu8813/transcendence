cfe12da15fa1cbbd10901c92e895bc931939c45a
hubble <38990435+hu8813@users.noreply.github.com>
2024-02-12 14:04:03 +0000

    a

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import i18n from '../../i18n';
-import { Form, Button, Container } from 'react-bootstrap';
-import { FiUser } from 'react-icons/fi';
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import i18n from "../../i18n";
+import { Form, Button, Container } from "react-bootstrap";
+import { FiUser } from "react-icons/fi";
 
-import './Login.css'; // Create a Login.css file for styling
+import "./Login.css"; // Create a Login.css file for styling
 // Import necessary libraries and components
 
 const Login = () => {
-  const [csrftoken, setCsrfToken] = useState('');
+  const [csrftoken, setCsrfToken] = useState("");
   const { t } = useTranslation();
   const location = useLocation();
-  const [authCode, setAuthCode] = useState(null); 
+  const [authCode, setAuthCode] = useState(null);
 
   useEffect(() => {
     fetchCsrfToken();
@@ -20,24 +20,25 @@ const Login = () => {
 
   const fetchCsrfToken = async () => {
     try {
-      const response = await fetch('/get-csrf-token/');
+      const response = await fetch("/get-csrf-token/");
       const data = await response.json();
-      const token = data.csrfToken || 'ns9y1mCcGwoeH5Sh4WTcJZfdg600L0nm'; // Set default value if token is empty
+      const token = data.csrfToken || "ns9y1mCcGwoeH5Sh4WTcJZfdg600L0nm"; // Set default value if token is empty
       setCsrfToken(token);
     } catch (error) {
-      console.error('Error fetching CSRF token:', error);
-      setCsrfToken('ns9y1mCcGwoeH5Sh4WTcJZfdg600L0nm'); // Set default value if fetch fails
+      console.error("Error fetching CSRF token:", error);
+      setCsrfToken("ns9y1mCcGwoeH5Sh4WTcJZfdg600L0nm"); // Set default value if fetch fails
     }
   };
-  
 
   const handleSignIn = (provider) => {
-    if (provider === '42') {
+    if (provider === "42") {
       // Redirect the user to the 42 API authorization URL
       // Use your actual client ID and redirect URI
       let clientId = process.env.REACT_APP_CLIENT_ID || "your-client-id";
-      let redirectUri = process.env.REACT_APP_REDIRECT_URI || "https://42dashboard.vercel.app/login/42/return";
-      
+      let redirectUri =
+        process.env.REACT_APP_REDIRECT_URI ||
+        "https://42dashboard.vercel.app/login/42/return";
+
       const authUrl = `https://api.intra.42.fr/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code`;
 
       window.location.href = authUrl;
@@ -47,14 +48,14 @@ const Login = () => {
   };
 
   useEffect(() => {
-    const storedLanguage = localStorage.getItem('language');
+    const storedLanguage = localStorage.getItem("language");
     if (storedLanguage) {
       i18n.changeLanguage(storedLanguage);
     }
 
     // Check if the callback URL contains an authorization code
     const urlParams = new URLSearchParams(location.search);
-    const code = urlParams.get('code');
+    const code = urlParams.get("code");
 
     if (code) {
       // If an authorization code is present, set it in state
@@ -67,38 +68,43 @@ const Login = () => {
   const exchangeCodeForAccessToken = (code) => {
     // Make a POST request to exchange the code for an access token
     let clientId = process.env.REACT_APP_CLIENT_ID || "your-client-id";
-    let clientSecret = process.env.REACT_APP_CLIENT_SECRET || "your-client-secret";
-    let redirectUri = process.env.REACT_APP_REDIRECT_URI || "https://42dashboard.vercel.app/login/42/return";
-  
+    let clientSecret =
+      process.env.REACT_APP_CLIENT_SECRET || "your-client-secret";
+    let redirectUri =
+      process.env.REACT_APP_REDIRECT_URI ||
+      "https://42dashboard.vercel.app/login/42/return";
+
     const requestBody = new URLSearchParams();
-    requestBody.append('client_id', clientId);
-    requestBody.append('client_secret', clientSecret);
-    requestBody.append('code', code);
-    requestBody.append('redirect_uri', redirectUri);
-    requestBody.append('grant_type', 'authorization_code');
-  
-    fetch('https://api.intra.42.fr/oauth/token', {
-      method: 'POST',
+    requestBody.append("client_id", clientId);
+    requestBody.append("client_secret", clientSecret);
+    requestBody.append("code", code);
+    requestBody.append("redirect_uri", redirectUri);
+    requestBody.append("grant_type", "authorization_code");
+
+    fetch("https://api.intra.42.fr/oauth/token", {
+      method: "POST",
       body: requestBody,
     })
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         // Handle the API response, which will include an access token
         const accessToken = data.access_token;
         // Store the access token in a secure way, e.g., in local storage or state
-        localStorage.setItem('access_token', accessToken);
-  
+        localStorage.setItem("access_token", accessToken);
+
         // Debugging: Log the access token to the console
-        console.log('Access Token:', accessToken);
-  
+        console.log("Access Token:", accessToken);
+
         // Now you can use the access token for authenticated requests
       })
-      .catch(error => {
+      .catch((error) => {
         // Handle any errors that occur during the token exchange
-        console.error('Error exchanging authorization code for access token:', error);
+        console.error(
+          "Error exchanging authorization code for access token:",
+          error,
+        );
       });
   };
-  
 
   return (
     <Container>
@@ -106,35 +112,53 @@ const Login = () => {
         <Button
           variant="contained"
           color="primary"
-          onClick={() => handleSignIn('42')}
+          onClick={() => handleSignIn("42")}
           className="rounded focus:outline-none hover:bg-blue-600 mb-4"
         >
           <FiUser className="mr-2" />
-          {t('auth.signInWith42')}
+          {t("auth.signInWith42")}
         </Button>
         <div className="or-separator mb-4">
-          <span className="or-text">{t('common.or')}</span>
+          <span className="or-text">{t("common.or")}</span>
         </div>
-        <div className="text-center mt-4 name">
-          {t('auth.signIn')}
-        </div>
-     
+        <div className="text-center mt-4 name">{t("auth.signIn")}</div>
 
-        <form className="p-3 mt-3" method="post" action="https://four2trans-backend.onrender.com/login/">
-          <input type="hidden" name="csrfmiddlewaretoken" value={csrftoken} />
-          <div className="form-field d-flex align-items-center">
-            <span className="far fa-user"></span>
-            <input type="text" name="username" id="userName" placeholder={t('auth.email')} />
-          </div>
-          <div className="form-field d-flex align-items-center">
-            <span className="fas fa-key"></span>
-            <input type="password" name="password" id="pwd" placeholder={t('auth.password')} />
-          </div>
-          <Button type="submit" className="btn mt-3">{t('auth.login')}</Button>
-        </form>
+        {csrftoken && ( // Conditionally render the form if csrftoken is not empty
+          <form
+            className="p-3 mt-3"
+            method="post"
+            action="https://four2trans-backend.onrender.com/login/"
+          >
+            <input type="hidden" name="csrfmiddlewaretoken" value={csrftoken} />
+            <div className="form-field d-flex align-items-center">
+              <span className="far fa-user"></span>
+              <input
+                type="text"
+                name="username"
+                id="userName"
+                placeholder={t("auth.username")}
+                value="demo"
+              />
+            </div>
+            <div className="form-field d-flex align-items-center">
+              <span className="fas fa-key"></span>
+              <input
+                type="password"
+                name="password"
+                id="pwd"
+                placeholder={t("auth.password")}
+                value="demo"
+              />
+            </div>
+            <Button type="submit" className="btn mt-3">
+              {t("auth.login")}
+            </Button>
+          </form>
+        )}
 
         <div className="text-center fs-6">
-          <Link to="/forgot-password">{t('auth.forgotPassword')}</Link> {t('common.or')} <Link to="/register">{t('auth.registerHere')}</Link>
+          <Link to="/forgot-password">{t("auth.forgotPassword")}</Link>{" "}
+          {t("common.or")} <Link to="/register">{t("auth.registerHere")}</Link>
         </div>
       </div>
     </Container>
--- src/i18n.js
+++ src/i18n.js
@@ -1,286 +1,40 @@
 // i18n.js
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
 
-i18n
-  .use(initReactI18next)
-  .init({
-    resources: {
-      en: {
-        translation: {
-          home: {
-            welcome: 'Welcome to the Ping Pong Game!',
-          },
-          auth: {
-            email: 'Email',
-            enterEmail: 'Enter your email',
-            password: 'Password',
-            enterPassword: 'Enter your password',
-            rememberMe: 'Remember me',
-            login: 'Login',
-            forgotPassword: 'Forgot your password?',
-            notRegistered: 'Not registered yet?',
-            registerHere: 'Register here',
-            register: 'Register',
-            signInWith42: 'Login via 42Sign-in',
-            signIn: 'Login',
-            passwordMismatch: 'Password empty/invalid',
-            confirmPassword: 'Confirm password',
-            termsAndConditions: 'Terms & Conditions',
-            acceptTerms: 'i accept Terms & Conditions'
-            
-          },
-          game: {
-            title: 'Play!',
-          },
-          leaderboard: {
-            title: 'Leaderboard',
-          },
-          chat: {
-            title: 'Chat',
-          },
-          common: {
-            home: 'Home',
-            switchLanguage: 'Switch Language',
-            or: 'or',
-          },
-          footer: {
-            privacyPolicy: 'Privacy Policy',
-            impressum: 'Impressum',
-            madeWith: 'Made with',
-            at42Vienna: 'at 42Vienna',
-          },
-        },
-      },
-      fr: {
-        translation: {
-          home: {
-            welcome: 'Bienvenue dans l\'application Ping Pong!',
-          },
-          auth: {
-            email: 'E-mail',
-            enterEmail: 'Entrez votre adresse e-mail',
-            password: 'Mot de passe',
-            enterPassword: 'Entrez votre mot de passe',
-            rememberMe: 'Se souvenir de moi',
-            login: 'Connexion',
-            forgotPassword: 'Mot de passe oublié?',
-            notRegistered: 'Pas encore inscrit?',
-            registerHere: 'Inscrivez-vous ici',
-            passwordMismatch: 'Mot de passe vide/invalide',
-          confirmPassword: 'Confirmer le mot de passe',
-          termsAndConditions: 'Termes et conditions',
-          acceptTerms: 'J\'accepte les termes et conditions',
-          },
-          game: {
-            title: 'Jeu de Ping Pong',
-          },
-          leaderboard: {
-            title: 'Classement',
-          },
-          chat: {
-            title: 'Discussion',
-          },
-          common: {
-            home: 'Accueil',
-            switchLanguage: 'Changer la langue',
-          },
-          footer: {
-            privacyPolicy: "Politique de confidentialité",
-            impressum: "Mentions légales",
-            madeWith: "Fabriqué avec",
-            at42Vienna: "chez 42Vienna",
-            
-          }
-        },
-      },
-      tr: {
-        translation: {
-          home: {
-            welcome: 'Ping Pong Oyununa Hoş Geldin!',
-          },
-          auth: {
-            email: 'E-posta',
-            enterEmail: 'E-posta adresinizi girin',
-            password: 'Şifre',
-            enterPassword: 'Şifrenizi girin',
-            rememberMe: 'Beni hatırla',
-            login: 'Giriş yap',
-            forgotPassword: 'Şifrenizi mi unuttunuz?',
-            notRegistered: 'Henüz kayıtlı değil misiniz?',
-            registerHere: 'Buradan kaydolun',
-            register: 'Kayıt ol',
-            signInWith42: '42Signin ile girin',
-            signIn: 'Giriş yap',
-            passwordMismatch: 'Şifre boş/geçersiz',
-            confirmPassword: 'Şifreyi Onayla',
-            termsAndConditions: 'Koşullar ve Şartlar',
-            acceptTerms: 'Şartları ve Koşulları Kabul Ediyorum',
-          },
-          game: {
-            title: 'Oyna!',
-          },
-          leaderboard: {
-            title: 'Liderlik Tablosu',
-          },
-          chat: {
-            title: 'Sohbet',
-          },
-          common: {
-            home: 'Anasayfa',
-            or: 'veya',
-            switchLanguage: 'Dil Değiştir',
-          },
-          footer: {
-            privacyPolicy: "Gizlilik Politikası",
-            impressum: "İletişim",
-            madeWith: "",
-            at42Vienna: "42Vienna'da"
-          }
-        },
-      },
-      bg: {
-        translation: {
-          home: {
-            welcome: 'Добре дошли в Приложението на пинг-понг!',
-          },
-          auth: {
-            email: 'Имейл',
-            enterEmail: 'Въведете вашия имейл',
-            password: 'Парола',
-            enterPassword: 'Въведете вашия парола',
-            rememberMe: 'Запомни ме',
-            login: 'Вход',
-            forgotPassword: 'Забравена парола?',
-            notRegistered: 'Все още нямате регистрация?',
-            registerHere: 'Регистрирайте се тук',
-            register: 'Регистрация',
-            signInWith42: 'Влезте с 42Sign-in',
-            signIn: 'Вход',
-            passwordMismatch: 'Парола празна/невалидна',
-            confirmPassword: 'Потвърди парола',
-            termsAndConditions: 'Условия и правила',
-            acceptTerms: 'Приемам условията и правилата',
-          },
-          game: {
-            title: 'Играй!',
-          },
-          leaderboard: {
-            title: 'Таблица с резултати',
-          },
-          chat: {
-            title: 'Чат',
-          },
-          common: {
-            home: 'Начало',
-            or: 'или',
-            switchLanguage: 'Смени езика',
-          },
-          footer: {
-            privacyPolicy: "Политика за поверителност",
-            impressum: "Импресум",
-            madeWith: "Изработено с",
-            at42Vienna: "в 42Vienna"
-          }
-        },
-      },
-      eg: {
-        translation: {
-          home: {
-            welcome: 'مرحبًا بك في تطبيق لعبة كرة الطاولة!',
-          },
-          auth: {
-            email: 'البريد الإلكتروني',
-            enterEmail: 'أدخل بريدك الإلكتروني',
-            password: 'كلمة المرور',
-            enterPassword: 'أدخل كلمة المرور الخاصة بك',
-            rememberMe: 'تذكرني',
-            login: 'تسجيل الدخول',
-            forgotPassword: 'هل نسيت كلمة المرور؟',
-            notRegistered: 'ليس لديك حساب؟',
-            registerHere: 'سجل هنا',
-            register: 'تسجيل',
-            signInWith42: 'تسجيل الدخول بواسطة 42Sign-in',
-            signIn: 'تسجيل الدخول',
-            passwordMismatch: 'كلمة المرور فارغة/غير صالحة',
-            confirmPassword: 'تأكيد كلمة المرور',
-            termsAndConditions: 'الشروط والأحكام',
-            acceptTerms: 'أوافق على الشروط والأحكام',
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import enTranslations from "./translations/en.json";
+import frTranslations from "./translations/fr.json";
+import trTranslations from "./translations/tr.json";
+import bgTranslations from "./translations/bg.json";
+import egTranslations from "./translations/eg.json";
+import atTranslations from "./translations/at.json";
 
-          },
-          game: {
-            title: 'اللعب!',
-          },
-          leaderboard: {
-            title: 'الترتيب',
-          },
-          chat: {
-            title: 'الدردشة',
-          },
-          common: {
-            home: 'الرئيسية',
-            or: 'أو',
-            switchLanguage: 'تغيير اللغة',
-          },
-          footer: {
-            privacyPolicy: "سياسة الخصوصية",
-            impressum: "إشهار الموقع",
-            madeWith: "صُنع بواسطة",
-            at42Vienna: "في 42Vienna"
-          }
-        },
-      },
-    
-      at: {
-        translation: {
-          home: {
-            welcome: 'Willkommen in der Ping Pong Game App!',
-          },
-          auth: {
-            email: 'E-Mail',
-            enterEmail: 'Geben Sie Ihre E-Mail-Adresse ein',
-            password: 'Passwort',
-            enterPassword: 'Geben Sie Ihr Passwort ein',
-            rememberMe: 'Erinnere dich an mich',
-            login: 'Anmelden',
-            forgotPassword: 'Passwort vergessen?',
-            notRegistered: 'Noch nicht registriert?',
-            registerHere: 'Hier registrieren',
-            register: 'Registrieren',
-            passwordMismatch: 'Passwort leer/ungültig',
-            confirmPassword: 'Passwort bestätigen',
-            termsAndConditions: 'Geschäftsbedingungen',
-            acceptTerms: 'Ich akzeptiere die Geschäftsbedingungen',
-          },
-          game: {
-            title: 'Spiel es!',
-          },
-          leaderboard: {
-            title: 'Bestenliste',
-          },
-          chat: {
-            title: 'Chat',
-          },
-          common: {
-            home: 'Startseite',
-            switchLanguage: 'Sprache wechseln',
-            or: 'oder',
-          },
-          footer: {
-            privacyPolicy: "Datenschutz",
-            impressum: "Impressum",
-            madeWith: "Erstellt mit",
-            at42Vienna: "bei 42Vienna"
-          }
-          
-        },
-      },
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: enTranslations,
     },
-    lng: localStorage.getItem('language') || 'en', // Get the language from localStorage
-    fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false,
+    fr: {
+      translation: frTranslations,
     },
-  });
+    tr: {
+      translation: trTranslations,
+    },
+    bg: {
+      translation: bgTranslations,
+    },
+    eg: {
+      translation: egTranslations,
+    },
+    at: {
+      translation: atTranslations,
+    },
+  },
+  lng: localStorage.getItem("language") || "en",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
