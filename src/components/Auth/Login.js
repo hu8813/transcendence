3255c52c769c773474c52898195b4cfabff12583
hubble <38990435+hu8813@users.noreply.github.com>
2024-02-20 11:03:48 +0000

    a

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -140,7 +140,7 @@ const Login = ({ setLoggedIn }) => {
                 name="username"
                 id="userName"
                 placeholder={t("auth.username")}
-                defaultValue="demo"
+                defaultValue="user42"
               />
             </div>
             <div className="form-field d-flex align-items-center">
@@ -150,7 +150,7 @@ const Login = ({ setLoggedIn }) => {
                 name="password"
                 id="pwd"
                 placeholder={t("auth.password")}
-                defaultValue="demo"
+                defaultValue="Pass@42"
               />
             </div>
             <Button type="submit" className="btn mt-3">
