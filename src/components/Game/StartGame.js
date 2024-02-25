79fb2469512fbb3a04ef2079a7325ffea8242ea6
hubble <38990435+hu8813@users.noreply.github.com>
2024-02-25 10:24:17 +0000

    added Play page style

--- src/components/Game/StartGame.js
+++ src/components/Game/StartGame.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import PongGame from "./PongGame";
-import Player3D1 from "./Player3D1";
-import PlayerAi1 from "./PlayerAi1";
-import PlayersRemote2 from "./PlayersRemote2";
+import {
+  IoGameControllerOutline,
+  IoGameControllerSharp,
+  IoGameController,
+} from "react-icons/io5";
+import "./StartGame.css"; // Import CSS file for custom styling if needed
 
 const StartGame = () => {
   const [selectedGame, setSelectedGame] = useState("");
@@ -13,54 +15,49 @@ const StartGame = () => {
   };
 
   return (
-    <div className="container mt-5">
+    <div className="container mt-5 text-center">
+      {" "}
+      {/* Updated to include text-center class */}
       <div className="row justify-content-center">
         <div className="col-md-6">
-          <h1 className="text-center mb-4">Select a Game</h1>
+          <h1 className="mb-4">Select a Game:</h1>{" "}
+          {/* Removed text-center class */}
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
-              <Link to="/PongGame" className="text-decoration-none">
-                <button
-                  className="btn btn-primary w-100 d-flex justify-content-between align-items-center"
-                  onClick={() => handleGameSelection("PongGame")}
-                >
-                  <span>Standart Game (2 Players locally)</span>
-                  <i className="bi bi-controller"></i>
-                </button>
-              </Link>
+              <button
+                className="btn btn-primary btn-lg w-100 d-flex justify-content-between align-items-center"
+                onClick={() => handleGameSelection("PongGame")}
+              >
+                <span>Standart Game (2 Players locally)</span>
+                <IoGameControllerOutline />
+              </button>
             </li>
             <li className="list-group-item">
-              <Link to="/Player3D1" className="text-decoration-none">
-                <button
-                  className="btn btn-primary w-100 d-flex justify-content-between align-items-center"
-                  onClick={() => handleGameSelection("Player3D1")}
-                >
-                  <span>1 Player 3D</span>
-                  <i className="bi bi-joystick"></i>
-                </button>
-              </Link>
+              <button
+                className="btn btn-primary btn-lg w-100 d-flex justify-content-between align-items-center"
+                onClick={() => handleGameSelection("Player3D1")}
+              >
+                <span>1 Player 3D</span>
+                <IoGameControllerSharp />
+              </button>
             </li>
             <li className="list-group-item">
-              <Link to="/PlayerAi1" className="text-decoration-none">
-                <button
-                  className="btn btn-primary w-100 d-flex justify-content-between align-items-center"
-                  onClick={() => handleGameSelection("PlayerAi1")}
-                >
-                  <span>1 Player with AI</span>
-                  <i className="bi bi-joystick"></i>
-                </button>
-              </Link>
+              <button
+                className="btn btn-primary btn-lg w-100 d-flex justify-content-between align-items-center"
+                onClick={() => handleGameSelection("PlayerAi1")}
+              >
+                <span>1 Player with AI</span>
+                <IoGameControllerSharp />
+              </button>
             </li>
             <li className="list-group-item">
-              <Link to="/PlayersRemote2" className="text-decoration-none">
-                <button
-                  className="btn btn-primary w-100 d-flex justify-content-between align-items-center"
-                  onClick={() => handleGameSelection("PlayersRemote2")}
-                >
-                  <span>2 Players Remotely</span>
-                  <i className="bi bi-wifi"></i>
-                </button>
-              </Link>
+              <button
+                className="btn btn-primary btn-lg w-100 d-flex justify-content-between align-items-center"
+                onClick={() => handleGameSelection("PlayersRemote2")}
+              >
+                <span>2 Players Remotely</span>
+                <IoGameController />
+              </button>
             </li>
           </ul>
         </div>
