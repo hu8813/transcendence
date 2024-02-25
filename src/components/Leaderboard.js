b9dbbfd75d9c07e788bdc343460abb4a908883a1
hubble <38990435+hu8813@users.noreply.github.com>
2024-02-25 12:09:12 +0000

    a

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,74 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import "./Leaderboard.css"; // Import the CSS file
 
 const Leaderboard = () => {
   const { t } = useTranslation();
+  const [leaderboardData, setLeaderboardData] = useState([]);
 
-  // Team data
-  const team = [
-    {
-      rank: 1,
-      name: "Lewis Hamilton",
-      img: "https://www.formula1.com/content/dam/fom-website/drivers/L/LEWHAM01_Lewis_Hamilton/lewham01.png.transform/2col-retina/image.png",
-      kudos: 36,
-    },
-    {
-      rank: 2,
-      name: "Kimi Raikkonen",
-      img: "https://www.formula1.com/content/dam/fom-website/drivers/K/KIMRAI01_Kimi_R%C3%A4ikk%C3%B6nen/kimrai01.png.transform/2col-retina/image.png",
-      kudos: 31,
-    },
-    {
-      rank: 3,
-      name: "Sebastian Vettel",
-      img: "https://www.formula1.com/content/dam/fom-website/drivers/S/SEBVET01_Sebastian_Vettel/sebvet01.png.transform/2col-retina/image.png",
-      kudos: 24,
-    },
-    {
-      rank: 4,
-      name: "Max Verstappen",
-      img: "https://www.formula1.com/content/dam/fom-website/drivers/M/MAXVER01_Max_Verstappen/maxver01.png.transform/2col-retina/image.png",
-      kudos: 20,
-    },
-    {
-      rank: 5,
-      name: "Valtteri Bottas",
-      img: "https://www.formula1.com/content/dam/fom-website/drivers/V/VALBOT01_Valtteri_Bottas/valbot01.png.transform/2col-retina/image.png",
-      kudos: 18,
-    },
-    {
-      rank: 6,
-      name: "Daniel Ricciardo",
-      img: "https://www.formula1.com/content/dam/fom-website/drivers/D/DANRIC01_Daniel_Ricciardo/danric01.png.transform/2col-retina/image.png",
-      kudos: 15,
-    },
-    {
-      rank: 7,
-      name: "Charles Leclerc",
-      img: "https://www.formula1.com/content/dam/fom-website/drivers/C/CHALEC01_Charles_Leclerc/chalec01.png.transform/2col-retina/image.png",
-      kudos: 12,
-    },
-    {
-      rank: 8,
-      name: "Carlos Sainz",
-      img: "https://www.formula1.com/content/dam/fom-website/drivers/C/CARSAI01_Carlos_Sainz/carsai01.png.transform/2col-retina/image.png",
-      kudos: 10,
-    },
-    {
-      rank: 9,
-      name: "Lando Norris",
-      img: "https://www.formula1.com/content/dam/fom-website/drivers/L/LANNOR01_Lando_Norris/lannor01.png.transform/2col-retina/image.png",
-      kudos: 8,
-    },
-    {
-      rank: 10,
-      name: "Fernando Alonso",
-      img: "https://www.formula1.com/content/dam/fom-website/drivers/F/FERALO01_Fernando_Alonso/feralo01.png.transform/2col-retina/image.png",
-      kudos: 6,
-    },
-    // Add more team members as needed
-  ];
+  useEffect(() => {
+    fetch("https://four2trans-backend.onrender.com/leaderboard/")
+      .then((response) => response.json())
+      .then((data) => setLeaderboardData(data))
+      .catch((error) =>
+        console.error("Error fetching leaderboard data:", error),
+      );
+  }, []);
+
+  // Function to generate a random unique color
+  const getRandomColor = () => {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  };
 
   return (
     <div className="l-wrapper">
@@ -82,22 +32,28 @@ const Leaderboard = () => {
           </tr>
         </thead>
         <tbody>
-          {team.map((member, index) => (
+          {leaderboardData.map((member, index) => (
             <tr key={index}>
-              <td>{member.rank}</td>
+              <td>{index + 1}</td>
               <td>
                 <div className="c-media">
-                  <img
+                  <div
                     className="c-avatar c-media__img"
-                    src={member.img}
-                    alt={member.name}
-                  />
+                    style={{ backgroundColor: getRandomColor() }}
+                  >
+                    {member.img ? (
+                      <img src={member.img} alt={member.username} />
+                    ) : (
+                      <div className="default-profile-pic"></div>
+                    )}
+                  </div>
                   <div className="c-media__content">
-                    <div className="c-media__title">{member.name}</div>
+                    <div className="c-media__title">{member.username}</div>
                   </div>
                 </div>
               </td>
-              <td>{member.kudos}</td>
+              <td>{member.score || 0}</td>{" "}
+              {/* Display 0 if score is empty or 0 */}
             </tr>
           ))}
         </tbody>
